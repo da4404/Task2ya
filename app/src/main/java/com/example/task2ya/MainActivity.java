f13b93e9895ba718ef8822511020e7584ed314ab
בר אופק <u9054555@bsmch.net>
2025-09-28 00:33:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task2ya/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.task2ya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText edit1;
+    EditText edit2;
+    EditText edit3;
+    TextView tV1;
+    private static final int REQUEST_CODE = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         edit1 = findViewById(R.id.number1);
+         edit2 = findViewById(R.id.number2);
+        edit3 = findViewById(R.id.number3);
+        tV1 = findViewById(R.id.answer);
+
+
+
+    }
+    @Override
+    protected void onActivityResult(int source, int result, @Nullable Intent data_back)
+    {
+        super.onActivityResult(source, result, data_back);
+        if (source == REQUEST_CODE)
+        {
+            if (result == RESULT_OK)
+            {
+                if (data_back != null)
+                {
+                    String count3 = data_back.getStringExtra("solve");
+                    tV1.setText(count3);
+                }
+            }
+        }
+    }
+    public void rand(View view)
+    {
+        Random rand = new Random();
+        int n1 = rand.nextInt(10);
+        int n2 = rand.nextInt(10);
+        int n3 = rand.nextInt(10);
+        edit1.setText(String.valueOf(n1));
+        edit2.setText(String.valueOf(n2));
+        edit3.setText(String.valueOf(n3));
+    }
+
+    public void calc(View view)
+    {
+        if(!(edit1.getText().toString().isEmpty() || edit2.getText().toString().isEmpty() || edit3.getText().toString().isEmpty() )) {
+
+            Intent getNumber = new Intent(this, calculate.class);
+            double a = Double.valueOf(edit1.getText().toString());
+            double b = Double.valueOf(edit2.getText().toString());
+            double c = Double.valueOf(edit3.getText().toString());
+
+            getNumber.putExtra("a", a);
+            getNumber.putExtra("b", b);
+            getNumber.putExtra("c", c);
+            startActivityForResult(getNumber, REQUEST_CODE);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/task2ya/calculate.java
@@ -0,0 +1,110 @@
+package com.example.task2ya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class calculate extends AppCompatActivity
+{
+    ImageView parabulal;
+    Button returnBack;
+    String result;
+    TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate);
+        double a = getIntent().getDoubleExtra("a", 0);
+        double b = getIntent().getDoubleExtra("b", 0);
+        double c = getIntent().getDoubleExtra("c", 0);
+        parabulal = findViewById(R.id.parabulal);
+        returnBack  = findViewById(R.id.back);
+
+        txtResult = findViewById(R.id.solvee);
+        result = solve(a, b, c);
+        txtResult.setText(result);
+
+    }
+
+
+    public String solve(double a, double b, double c)
+    {
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                if (c == 0)
+                {
+                    return "Infinite solutions";
+                }
+                else
+                {
+                    return "No solution";
+                }
+            }
+            else
+            {
+                double x = -c / b;
+                return "Single solution: x = " + x;
+            }
+        }
+
+        double delta = b * b - 4 * a * c;
+
+        if (delta < 0)
+        {
+            if (a > 0)
+            {
+                parabulal.setImageResource(R.drawable.parabola_up_no_root);
+            }
+            else
+            {
+                parabulal.setImageResource(R.drawable.parabola_down_no_root);
+            }
+            return "No real solutions";
+        }
+        else if (delta == 0)
+        {
+            if (a > 0)
+            {
+                parabulal.setImageResource(R.drawable.parabulal_up_one_root);
+            }
+            else
+            {
+                parabulal.setImageResource(R.drawable.parabola_down_one_root);
+            }
+            double x = -b / (2 * a);
+            return "Single solution: x = " + x;
+        }
+        else
+        {
+            if (a > 0)
+            {
+                parabulal.setImageResource(R.drawable.parabola_up_two_roots);
+            }
+            else
+            {
+                parabulal.setImageResource(R.drawable.parabola_down_two_root);
+            }
+            double sqrtDelta = Math.sqrt(delta);
+            double x1 = (-b + sqrtDelta) / (2 * a);
+            double x2 = (-b - sqrtDelta) / (2 * a);
+            return "Two solutions: x1 = " + x1 + ", x2 = " + x2;
+        }
+    }
+
+    public void back1(View view)
+    {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("solve", result);
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
+}
